d9f6cfbd6b451873b2ccac8d9d838d8eecc7040e
Almabade1 <balcedamariana@gmail.com>
2025-02-19 23:45:22 -0500

    Perfiles de tiendas de artesanos

--- src/pages/ArtisanShopProfiles/ArtisanShopProfiles.jsx
+++ src/pages/ArtisanShopProfiles/ArtisanShopProfiles.jsx
@@ -1,4 +1,5 @@
 import { useContextArtisanShop } from "../../hooks/useContextArtisanShop";
+import "./ArtisanShopProfiles.css";
 
 export const ArtisanShopProfiles = () => {
   const { responseArtisanShops, loadingArtisanShops, errorArtisanShops } =
@@ -17,12 +18,31 @@ export const ArtisanShopProfiles = () => {
     return <p>{errorArtisanShops}</p>;
   }
 
-  if (responseArtisanShops) {
-    return (
-      <section>
-        Perfil de las tiendas de los Artesanos:{" "}
-        {JSON.stringify(responseArtisanShops)}
-      </section>
-    );
+  if (!responseArtisanShops || responseArtisanShops.length === 0) {
+    return <p>No hay tiendas disponibles.</p>;
   }
+
+  return (
+    <section className="ArtisanShopProfiles">
+      {responseArtisanShops.map((shop) => (
+        <div key={shop.id} className="ArtisanShopProfiles_box">
+          <div className="ArtisanShopProfiles_image">
+            <img src={shop.image} alt={shop.name} />
+          </div>
+
+          <div className="ArtisanShopProfiles_data">
+            <h3><strong>{shop.name}</strong></h3>
+            <h5><strong>{shop.artisan}</strong></h5>
+            <p>{shop.message}</p>
+            <button 
+              className="ArtisanShopProfiles_button" 
+              onClick={() => navigate(`/tienda/${shop.id}`)}
+            >
+              Ver Tienda
+            </button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
